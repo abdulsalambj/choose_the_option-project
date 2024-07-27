3f1084919f1228825b7d8ec5ae88d3fd2bc47db7
abdulsalambj <abdulsalambj11@gmail.com>
2024-07-27 11:03:16 +0500

    Add files via upload

---
+++ Project_1.js
@@ -0,0 +1,25 @@
+
+
+let opt1= document.querySelector("#option1");
+opt1.addEventListener("click", (e) =>{
+
+
+    alert("Correct Answar")
+
+})
+
+let opt2 = document.querySelector("#option2");
+opt2.addEventListener('click', () =>{
+    alert("wrong Answar");
+})
+
+let opt3 = document.querySelector("#option3") ;
+opt3.addEventListener('click', () =>{
+    alert("Wrong Answar");
+
+})
+
+let opt4 = document.querySelector("#option4") ;
+opt4.addEventListener("click", () =>{
+    alert("Wrong Answar");
+} )
